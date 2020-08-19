9c8d227e6071de1cb5861aaab6fa9d711f1ffc39
therockspush <therockspush@users.noreply.github.com>
2020-08-19 16:36:21 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,18 @@ variable "password" {
    description = "password"
    type = string
    }
+
+variable "region" {
+    description = "The AWS region to deploy this module in"
+    type = string
+}
+
+variable "cidr" {
+    description = "The CIDR range to be used for the VPC"
+    type = string
+}
+
+variable "aws_account_name" {
+    description = "The AWS account name, as known by the Aviatrix controller"
+    type = string
+}
